33fa6d258741ab67237e854ecaba392f83031b88
Alex A. <frailxup@gmail.com>
2025-10-01 22:29:50 -0400

    Modified cameraOffset in CameraMovement to make it closer to the player.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public GameObject player;
-    Vector3 cameraOffset = new(0, 3, -8);
+    Vector3 cameraOffset = new(0, 2, -5);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
