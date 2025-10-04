398dd1231bba425a2618dfe53a90c2aababd0999
Alex A. <frailxup@gmail.com>
2025-10-04 15:18:06 -0400

    CameraMovement references player singleton.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    public GameObject player;
     Vector3 cameraOffset = new(0, 2, -5);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +12,6 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-      transform.position = player.transform.position + cameraOffset;
+      transform.position = Player.Instance.transform.position + cameraOffset;
     }
 }
