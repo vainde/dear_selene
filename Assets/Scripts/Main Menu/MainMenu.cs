e81d797eb0d9d66ae79985b00bd17aacf6dde730
Alex A. <frailxup@gmail.com>
2025-10-05 21:58:50 -0400

    Player selecting new game transfers them to the world.

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewGame : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+      SceneManager.LoadScene("Street");
+    }
+}
